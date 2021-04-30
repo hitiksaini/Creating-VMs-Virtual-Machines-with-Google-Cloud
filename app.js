cb006d8593e9830c3dfb935b326856c13ba6cad0
hitiksaini <hitik9045saini@gmail.com>
2021-04-30 07:03:35 +0530

    final demo ready

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// ********* Hitik Saini https://hitik20.tech/************
+//********************************************************
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
@@ -11,15 +14,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// mongo_access = process.env.MONGO_PASS;
-// mongoose.connect("mongodb+srv://hitiksaini:"+mongo_access+"@mozillachdevents.lncbn.mongodb.net/mozDB?retryWrites=true&w=majority",
-// { useUnifiedTopology: true, useNewUrlParser: true});
+//Mongo Atlas Connection
+mongo_access = process.env.MONGO_PASS;
+mongoose.connect("mongodb+srv://hitik:"+mongo_access+"@oneshotdemo.vgtre.mongodb.net/test",
+{ useUnifiedTopology: true, useNewUrlParser: true});
 
-mongoose.connect("mongodb://localhost:27017/test", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
 
+// mongoose.connect("mongodb://localhost:27017/test", {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true
+// });
+
+
+//Creating Schema
 const collegeSchema = {
   id: Number,
   name: String,
@@ -33,23 +40,27 @@ const collegeSchema = {
   }]
 };
 
+//model
 const College = mongoose.model("College", collegeSchema);
 
-const studentSchema = {
-  id: Number,
-  College_id: String,
-  name: String,
-  year_of_branch: Number,
-  skills: [{
-    type: String
-  }]
-};
+//use later
 
-const Student = mongoose.model("Student", studentSchema);
+// const studentSchema = {
+//   id: Number,
+//   College_id: String,
+//   name: String,
+//   year_of_branch: Number,
+//   skills: [{
+//     type: String
+//   }]
+// };
+//
+// const Student = mongoose.model("Student", studentSchema);
 
-
+//Setting routes with express
 app.get("/", function(req, res)
 {
+  //Show all db data as list on home page
   College.find({}, function(err, colleges){
     res.render("home", {colleges: colleges});
   });
@@ -57,49 +68,48 @@ app.get("/", function(req, res)
 });
 
 
-app.get("/chart", function(req, res){
-  res.render("chart");
-})
-
+// After search query, pulling Similar Colleges
 app.post("/", function(req, res) {
   const collegename = req.body.college_name;
   //Finding a college by its name or ID.
   College.find({
-    name: { "$regex" : collegename , "$options" : "i"}
+    "$or": [
+      { name: { "$regex" :collegename , "$options" : "i"}},
+      { state: { "$regex" :collegename , "$options" : "i"}},
+    ]
   }, function(err, foundItem) {
     if (foundItem.length === 0) {
-      res.send("<h1>Nothing Found, Please search a valid college</h1>");
-    } else {
-      console.log(foundItem);
-      console.log(foundItem.length);
-      // res.write("<h3> Colleges based on your search :</h3>");
-      for(var i = 0; i < foundItem.length; i++){
-        res.write("<b>Name : </b>" + foundItem[i].name + "<br>" +
-        "<b>Location : </b>" + foundItem[i].city + ", "+ foundItem[i].state + ", "+ foundItem[i].country +
-        "<br><b>Number of Students : </b>" + foundItem[i].num_of_students +
-        "<br><b>year Founded : </b>" + foundItem[i].year_founded +
-        "<br><b>Courses Offered : </b>" + foundItem[i].courses + "<br>" + "<br>"
-      );
-      }
+      res.render("err");
+    }
+    else {
+      res.render("search", {results: foundItem, cn: collegename});
     }
   });
 });
 
-
-//pulling Similar Colleges
-app.get("/filter", function(req, res) {
-  //Filter by location(state)
-  College.find({
-    state: "Punjab"
-  }, function(err, result) {
-    if (err) throw err;
-    console.log(result);
+// Visualisations
+app.get("/chart", function(req, res){
+  College.find({}, function(err, colleges){
+    res.render("chart", {colleges: colleges});
   });
-});
+})
 
 
 
+// app.get("/filter", function(req, res) {
+//   //Filter by location(state)
+//   College.find({
+//     state: "Punjab"
+//   }, function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
 
-app.listen(3000, function(req, res) {
-  console.log("listening on POrt 3000.");
+// encounter the error page
+app.get('*', (req, res) => res.send("<h1>Not found!</h1>"));
+
+// Port allocation
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server running sucessfully on port 3000.")
 });
